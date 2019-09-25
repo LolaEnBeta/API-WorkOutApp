db534ed8dc09b8a141a34de42217d0a0ae3b93ac
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-25 16:59:53 +0200

    Remove get_by(id) from ActivityRepository

--- ActivityRepository.py
+++ ActivityRepository.py
@@ -66,26 +66,6 @@ def get_by_type(type):
     else:
         return "An error has ocurred"
 
-def get_by(id):
-    conn = sqlite3.connect("sqlite3/database.db")
-    query = conn.cursor()
-
-    sql = 'SELECT * FROM activities WHERE id = %s' % id
-
-    if (query.execute(sql)):
-        row = query.fetchone()
-        if not row:
-            return None
-
-        activity = Activity(row[0], row[1], row[2], row[3], row[4], row[5])
-
-        query.close()
-        conn.commit()
-        conn.close()
-        return activity
-    else:
-        return "An error has ocurred"
-
 def remove(activity):
     conn = sqlite3.connect("sqlite3/database.db")
     query = conn.cursor()
